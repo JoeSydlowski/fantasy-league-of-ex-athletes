6e51cbff7717a5a5f95153909074e489600c05ee
Joe Sydlowski <syd235@gmail.com>
2021-09-06 13:11:38 -0500

    adding sims and cleaning folders

---
+++ _ffsim_2021.R
@@ -0,0 +1,25 @@
+
+# Libraries ---------------------------------------------------------------
+
+library(ffscrapr)
+library(tidyverse)
+library(ffsimulator)
+
+
+# Load Data ---------------------------------------------------------------
+
+flea_conn <- ff_connect(platform = "sleeper", league_id = "739986523688230912", season = 2021)
+
+ff_sim <- ff_simulate(flea_conn, n_seasons = 10000, gp_model = "simple", verbose = TRUE)
+
+png(file="plots/win_plot.png")
+autoplot(ff_sim, "wins") + tantastic::theme_tantastic()
+dev.off()
+
+png(file="plots/rank_plot.png")
+autoplot(ff_sim, "rank") + tantastic::theme_tantastic() + theme(legend.position="bottom")
+dev.off()
+
+png(file="plots/points_plot.png")
+autoplot(ff_sim, "points") + tantastic::theme_tantastic()
+dev.off()
