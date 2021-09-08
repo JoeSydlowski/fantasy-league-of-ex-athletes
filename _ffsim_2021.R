fa1e2e5ae501459245a13859d0b88a35c29c8fbb
Joe Sydlowski <syd235@gmail.com>
2021-09-07 23:39:40 -0500

    update ffsims

--- _ffsim_2021.R
+++ _ffsim_2021.R
@@ -10,13 +10,13 @@ library(ffsimulator)
 
 flea_conn <- ff_connect(platform = "sleeper", league_id = "739986523688230912", season = 2021)
 
-ff_sim <- ff_simulate(flea_conn, n_seasons = 10000, gp_model = "simple", verbose = TRUE)
+ff_sim <- ff_simulate(flea_conn, n_seasons = 10000, gp_model = "simple", verbose = TRUE, seed = 815)
 
 png(file="plots/win_plot.png")
 autoplot(ff_sim, "wins") + tantastic::theme_tantastic()
 dev.off()
 
-png(file="plots/rank_plot.png")
+png(file="plots/rank_plot.png", width = 800, height = 600)
 autoplot(ff_sim, "rank") + tantastic::theme_tantastic() + theme(legend.position="bottom")
 dev.off()
 
