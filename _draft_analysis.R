181bd4a8dd7e5b79a80045f4570486bb4b57c077
Joe Sydlowski <syd235@gmail.com>
2022-09-04 17:48:48 -0500

    update league history through 2021

--- _draft_analysis.R
+++ _draft_analysis.R
@@ -6,9 +6,36 @@ library(tidyverse)
 library(ffpros)
 
 
-# Load Data ---------------------------------------------------------------
+# Load Data 2021 ---------------------------------------------------------------
 
 flea_conn <- ff_connect(platform = "sleeper", league_id = "739986523688230912", season = 2021)
+
+flea_draft <- ff_draft(flea_conn) %>%
+  mutate(ovr_pick = row_number(),
+         pick_value = 1000*exp(-0.025 * ovr_pick))
+
+ffpros_ranks <- fp_rankings(page = "superflex-cheatsheets", sport = "nfl") %>%
+  left_join(select(dp_playerids(), fantasypros_id, sleeper_id), by = "fantasypros_id") %>% 
+  transmute(sleeper_id, rank, pos_rank, rank_value = 1000*exp(-0.025 * rank))
+
+flea_join <- 
+  flea_draft %>% 
+  left_join(ffpros_ranks, by = c("player_id"="sleeper_id")) %>% 
+  mutate(pick_diff = rank_value - pick_value) 
+
+worst_picks <- flea_join %>% 
+  group_by(franchise_name) %>%
+  slice_min(order_by = pick_diff, n = 1, with_ties = FALSE) %>%
+  ungroup()
+
+best_picks <- flea_join %>% 
+  group_by(franchise_name) %>%
+  slice_min(order_by = -pick_diff, n = 1, with_ties = FALSE) %>%
+  ungroup()
+
+# Load Data 2022 ---------------------------------------------------------------
+
+flea_conn <- ff_connect(platform = "sleeper", league_id = "863895883606056960", season = 2022)
 
 flea_draft <- ff_draft(flea_conn) %>%
   mutate(ovr_pick = row_number(),
---
+++ _league-history-2022.R
@@ -0,0 +1,339 @@
+# Libraries ---------------------------------------------------------------
+# pak::pak("ffverse/ffscrapr@dev")
+library(ffscrapr)
+library(tidyverse)
+library(gt)
+library(RColorBrewer)
+library(emoji)
+
+# Functions ---------------------------------------------------------------
+get_draft_history_espn <- 
+  function(season) {
+    conn <- espn_connect(
+      season = season,
+      league_id = 805175,
+      espn_s2 = Sys.getenv("FLEA_ESPN_S2"),
+      swid = Sys.getenv("FLEA_SWID"),
+      rate_limit_number = 1000,
+      rate_limit_seconds = 60
+    )
+    
+    df <- 
+      ff_draft(conn) %>% 
+      left_join(ff_franchises(conn) %>% select(franchise_id, user_name), by = "franchise_id")
+    
+    return(df)
+  }
+
+get_standings_history_espn <- 
+  function(season) {
+    conn <- espn_connect(
+      season = season,
+      league_id = 805175,
+      espn_s2 = Sys.getenv("FLEA_ESPN_S2"),
+      swid = Sys.getenv("FLEA_SWID"),
+      rate_limit_number = 1000,
+      rate_limit_seconds = 60
+    )
+    
+    df <- 
+      ff_standings(conn) %>% 
+      left_join(ff_franchises(conn) %>% select(franchise_id, user_name), by = "franchise_id")
+    
+    return(df)
+  }
+
+get_draft_history_sleeper <- 
+  function(season) {
+    conn <- sleeper_connect(
+      season = season,
+      league_id = 739986523688230912,
+      rate_limit_number = 1000,
+      rate_limit_seconds = 60
+    )
+    
+    df <- 
+      ff_draft(conn) %>% 
+      mutate(overall = row_number()) %>% 
+      left_join(ff_franchises(conn) %>% select(franchise_id, user_name), by = "franchise_id")
+    
+    return(df)
+  }
+
+get_standings_history_sleeper <- 
+  function(season) {
+    conn <- sleeper_connect(
+      season = season,
+      league_id = 739986523688230912,
+      rate_limit_number = 1000,
+      rate_limit_seconds = 60
+    )
+    
+    df <- 
+      ff_standings(conn) %>% 
+      left_join(ff_franchises(conn) %>% select(franchise_id, user_name), by = "franchise_id")
+    
+    return(df)
+  }
+
+get_headshot <- 
+  function(player_id, season){
+    
+    if(season <= 2020) {
+      nflreadr::load_rosters(season) %>% 
+        filter(espn_id == player_id) %>% 
+        pull(headshot_url)
+    } else {
+      nflreadr::load_rosters(season) %>% 
+        filter(sleeper_id == player_id) %>% 
+        pull(headshot_url)
+    }
+    
+  }
+
+
+# Data Pulls --------------------------------------------------------------
+espn_conn <- espn_connect(
+  season = 2021,
+  league_id = 805175,
+  espn_s2 = Sys.getenv("FLEA_ESPN_S2"),
+  swid = Sys.getenv("FLEA_SWID"),
+  rate_limit_number = 1000,
+  rate_limit_seconds = 60
+)
+
+sleeper_conn <- 
+  sleeper_connect(
+    season = 2021,
+    league_id = 739986523688230912,
+    rate_limit_number = 1000,
+    rate_limit_seconds = 60   
+  )
+
+espn_scoring_history <- 
+  ff_scoringhistory(espn_conn, season = 2015:2020) %>%
+  filter(week <= 16,
+         pos %in% c("QB","RB","WR","TE")) %>% 
+  rename(platform_id = espn_id)
+
+sleeper_scoring_history <- 
+  ff_scoringhistory(sleeper_conn, season = 2021) %>%
+  filter(week <= 17, pos %in% c("QB","RB","WR","TE")) %>% 
+  rename(platform_id = sleeper_id)
+
+scoring_history <- 
+  espn_scoring_history %>% 
+  bind_rows(sleeper_scoring_history) %>% 
+
+  group_by(season, platform_id, pos, gsis_id) %>% 
+  summarise(ppg = mean(points),
+            games = n()) %>%
+  ungroup() %>% 
+  group_by(season, pos) %>% 
+  mutate(ppg = if_else(games < 4, NA_real_, ppg),
+         ppg_rank = row_number(-ppg),
+         ppg_rank_value = 1000 * exp(-0.1 * ppg_rank),
+         games_played_percent = games/15) %>% 
+  ungroup() %>% 
+  filter(!is.na(platform_id)) %>% 
+  select(season, platform_id, ppg_rank, ppg_rank_value, games, games_played_percent)
+
+# Draft Skill
+espn_draft_history <- 
+  tibble(season = c(2015:2020)) %>% 
+  mutate(nested_draft = map(season, get_draft_history_espn)) %>% 
+  unnest(cols = nested_draft)
+
+sleeper_draft_history <- 
+  get_draft_history_sleeper(2021) %>% 
+  mutate(across(.cols = c(season, player_id),
+                .fns = as.integer))
+
+draft_history <- 
+  espn_draft_history %>% 
+  bind_rows(sleeper_draft_history) %>% 
+  group_by(season, pos) %>% 
+  mutate(pos_rank = row_number(overall),
+         pos_rank_value = 1000 * exp(-0.1 * pos_rank)) %>% 
+  mutate(player_id = as.character(player_id),
+         user_name = case_when(
+           user_name == "AnthonyClay" ~ "Anthony Ossola",
+           user_name == "EricBu" ~ "Eric Bulak",
+           user_name == "Galletti" ~ "Neal Galletti",
+           user_name == "jsydlow99ski" ~ "Jonathan Sydlowski",
+           user_name == "JTNoel" ~ "Josh Noel",
+           user_name == "LizandroS23" ~ "Lizandro Sanchez",
+           user_name == "nickd1212" ~ "Nick Duncan",
+           user_name == "rgalletti" ~ "Ryan Galletti",
+           user_name == "scottydontknovv" ~ "Scott Wilkens",
+           user_name == "sox05syd" ~ "Joe Sydlowski",
+           user_name == "terryclark1991" ~ "terry clark",
+           user_name == "TyNoel" ~ "Ty Noel",
+           TRUE ~ user_name)) %>% 
+  ungroup()
+
+draft_history_join <- 
+  draft_history %>% 
+  left_join(scoring_history, by = c("season", "player_id" = "platform_id")) %>%
+  mutate(value_diff = ppg_rank_value - pos_rank_value,
+         games_played_percent = replace_na(games_played_percent, 0),
+         injury_value_lost = pos_rank_value * (1-games_played_percent)) %>% 
+  filter(pos %in% c("QB","RB","WR","TE"))
+
+draft_values <- 
+  draft_history_join %>% 
+  group_by(user_name) %>% 
+  summarise(injury_value_oe = sum(injury_value_lost, na.rm = TRUE) / sum(pos_rank_value, na.rm = TRUE),
+            draft_value_oe= sum(ppg_rank_value, na.rm = TRUE) / sum(pos_rank_value, na.rm = TRUE) - 1) %>% 
+  ungroup()
+
+#Determine best and worst picks
+combine_word <- function(name, finish_string){
+  glue::glue(
+    "<div style='line-height:10px'><span style='font-weight:bold;font-variant:small-caps;font-size:14px'>{name}</div>
+        <div style='line-height:12px'><span style ='font-weight:bold;color:grey;font-size:10px'>{finish_string}</span></div>"
+  )
+}
+
+library(glue)
+best_draft_picks <-
+  draft_history_join %>%
+  group_by(user_name) %>%
+  slice_min(order_by = -value_diff, n = 1, with_ties = FALSE) %>%
+  ungroup() %>%
+  mutate(best_string = glue("Drafted {pos}{pos_rank} - Finshed {pos}{ppg_rank}"),
+         best_headshot = map2(player_id, season, get_headshot),
+         player_name = glue("{player_name} ({season})"),
+         best_combo = combine_word(player_name, best_string),
+         best_combo = map(best_combo, gt::html)) %>%
+  select(user_name, best_combo, best_headshot)
+
+worst_draft_picks <-
+  draft_history_join %>%
+  group_by(user_name) %>%
+  slice_min(order_by = value_diff, n = 1, with_ties = FALSE) %>%
+  ungroup() %>%
+  mutate(worst_string = glue("Drafted {pos}{pos_rank} - Finshed {pos}{ppg_rank}"),
+         worst_headshot = map2(player_id, season, get_headshot),
+         player_name = glue("{player_name} ({season})"),
+         worst_combo = combine_word(player_name, worst_string),
+         worst_combo = map(worst_combo, gt::html)) %>%
+  select(user_name, worst_combo, worst_headshot)
+
+top_draft_teams <-
+  draft_history_join %>%
+  group_by(user_name, team) %>%
+  summarise(avg_value = sum(pos_rank_value)) %>%
+  ungroup() %>%
+  group_by(user_name) %>%
+  arrange(-avg_value) %>%
+  slice_head(n = 1) %>%
+  ungroup() %>%
+  mutate(team = case_when(team == "NEP" ~ "NE",
+                          team == "GBP" ~ "GB",
+                          team == "KCC" ~ "KC",
+                          team == "NOS" ~ "NO",
+                          TRUE ~ team)) %>%
+  left_join(select(nflfastR::teams_colors_logos, team_abbr, team_logo_espn = team_logo_wikipedia), by = c("team" = "team_abbr")) %>%
+  select(user_name, team_logo_espn)
+
+#Draft History Plot
+draft_pick <- 
+  draft_history %>% 
+  group_by(season, user_name) %>% 
+  summarise(yearly_pick = min(overall)) %>% 
+  ungroup() %>%
+  mutate(pick_suffix = case_when(yearly_pick %in% c(11,12,13) ~ "th",
+                                 yearly_pick %% 10 == 1 ~ 'st',
+                                 yearly_pick %% 10 == 2 ~ 'nd',
+                                 yearly_pick %% 10 == 3 ~'rd',
+                                 TRUE ~ "th")) %>%
+  group_by(user_name) %>% 
+  summarise(pick_year = paste(yearly_pick, pick_suffix, sep = "", collapse = ", "),
+            years = n(),
+            last_season = max(season)) %>% 
+  ungroup()
+
+# Records
+# Draft Skill
+espn_standings_history <- 
+  tibble(season = c(2015:2020)) %>%
+  mutate(nested_records = map(season, get_standings_history_espn)) %>% 
+  unnest(cols = nested_records)
+
+sleeper_standings_history <- 
+  get_standings_history_sleeper(2021) %>% 
+  mutate(season = 2021,
+         league_rank = case_when(
+           user_name == "rgalletti" ~ 1,
+           user_name == "scottydontknovv" ~ 2,
+           user_name == "LizandroS23" ~ 3,
+           user_name == "EricBu" ~ 4,
+           user_name == "jsydlow99ski" ~ 5,
+           user_name == "terryclark1991" ~ 6,
+           user_name == "Galletti" ~ 7,
+           user_name == "nickd1212" ~ 8,
+           user_name == "sox05syd" ~ 9,
+           user_name == "JTNoel" ~ 10,
+           user_name == "AnthonyClay" ~ 11,
+           user_name == "TyNoel" ~ 12
+         ))
+
+standings_history_combined <-
+  espn_standings_history %>% 
+  bind_rows(sleeper_standings_history) %>% 
+  mutate(user_name = case_when(
+    user_name == "AnthonyClay" ~ "Anthony Ossola",
+    user_name == "EricBu" ~ "Eric Bulak",
+    user_name == "Galletti" ~ "Neal Galletti",
+    user_name == "jsydlow99ski" ~ "Jonathan Sydlowski",
+    user_name == "JTNoel" ~ "Josh Noel",
+    user_name == "LizandroS23" ~ "Lizandro Sanchez",
+    user_name == "nickd1212" ~ "Nick Duncan",
+    user_name == "rgalletti" ~ "Ryan Galletti",
+    user_name == "scottydontknovv" ~ "Scott Wilkens",
+    user_name == "sox05syd" ~ "Joe Sydlowski",
+    user_name == "terryclark1991" ~ "terry clark",
+    user_name == "TyNoel" ~ "Ty Noel",
+    TRUE ~ user_name))
+
+blank_emojis <- 
+  crossing(user_name = unique(standings_history_combined$user_name),
+           season = c(2015:2021))
+
+standings_history <- 
+  blank_emojis %>% 
+  full_join(standings_history_combined, by = c("user_name", "season")) %>%
+  mutate(finish_emoji = case_when(is.na(league_rank) ~ emoji("black_square_button"),
+                                  league_rank == 1 ~ emoji("trophy"),
+                                  league_rank == 2 ~ medal("silver"),
+                                  league_rank == 3 ~ medal("bronze"),
+                                  league_rank %in% c(4,5,6) ~ emoji("flexed_biceps"),
+                                  league_rank == 12 ~ emoji("taco"),
+                                  TRUE ~ emoji("wastebasket"))) %>% 
+  group_by(user_name) %>% 
+  summarise(across(.cols = c(h2h_wins, h2h_losses, h2h_ties, allplay_wins, allplay_losses),
+                   .fns = ~sum(.x, na.rm = TRUE)),
+            emoji_collase = paste(finish_emoji, sep = "", collapse = ""),
+            h2h_winpct = h2h_wins / (h2h_wins + h2h_losses + h2h_ties),
+            allplay_winpct = allplay_wins / (allplay_wins + allplay_losses),
+            first_place = sum(league_rank == 1, na.rm = TRUE),
+            luck_pct = h2h_winpct - allplay_winpct) %>% 
+  ungroup() %>% 
+  select(user_name, h2h_winpct, allplay_winpct, emoji_collase, luck_pct, first_place)
+
+# PLOTS
+flea_df <- 
+  draft_pick %>% 
+  # filter(last_season == 2020) %>%
+  inner_join(standings_history, by = "user_name") %>% 
+  inner_join(best_draft_picks, by = "user_name") %>%
+  inner_join(worst_draft_picks, by = "user_name") %>%
+  inner_join(top_draft_teams, by = "user_name") %>%
+  inner_join(draft_values, by = "user_name") %>% 
+  mutate(user_name = str_to_title(user_name)) %>% 
+  
+  arrange(-first_place, -allplay_winpct) %>% 
+  select(-c(first_place, last_season, pick_year))
+
+saveRDS(flea_df, "data/flea_df.RDS")
